06fc55e3a4d70e655afa0b5aef466deac15b0500
HuyNT <huyguainnt@gmail.com>
2025-08-27 01:11:47 +0700

    3. Webpack: babel for es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
 
 const config = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+            },
+        ],
     },
 };
 
