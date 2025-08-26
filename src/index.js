9bb5b39c812203b611f6bad102c20b4ce9552349
HuyNT <huyguainnt@gmail.com>
2025-08-27 01:53:07 +0700

    5. Webpack: Tạo project reactjs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import sum from './math';
 import './image';
 
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
 const total = sum(1, 2);
 
 console.log(total);
 document.write(total);
+
+const root = createRoot(document.getElementById('root'));
+root.render(<h1>Hello React</h1>);
